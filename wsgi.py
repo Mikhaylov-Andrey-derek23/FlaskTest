c96dd1134ed1d092545f3f1f2c7822322fcf649c
Mikhaylov-Andrey-derek23 <derek23@yandex.ru>
2019-02-25 01:10:15 +0300

    add

--- wsgi.py
+++ wsgi.py
@@ -30,6 +30,10 @@ def user(name):
    
     return response 
 
+@app.route("/nordic")
+def nordic():
+    return render_template("nordic.html")
+
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template("404.html"), 404
